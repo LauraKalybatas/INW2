6a84d7c8be4fbc0f6957649f49d2d7ff8b102848
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-19 11:37:48 -0300

    NODE

---
+++ node/funcao1.js
@@ -0,0 +1,51 @@
+function escreverNoConsole(){
+    console.log("Escrevendo na tela usando função!!")
+}
+
+//execução da função
+escreverNoConsole();
+
+const textoNoConsole = function(){
+    console.log("Segundo o exemplo, escrever no console");
+}
+
+textoNoConsole();
+
+const textoPorParametro = function(texto){
+    console.log(texto)
+}
+textoPorParametro("Terceiro exeplo, esse texto é o parametro!!!!")
+
+const textoUsandoArrow = texto => console.log(texto)
+textoUsandoArrow("QUarto exemplo usando arrow function")
+// const textoUsandoArrow = texto =>{
+//     console.log(texto)
+// }
+// textoUsandoArrow("QUarto exemplo usando arrow function") <--forma bruta
+
+//função arroq que recebe um numero e mostra na tela o numero e diz se é par ou impar
+
+const mostraParImpar = (numero) =>{
+    if(numero<0){
+        console.log(numero," o número é negativo")
+    }
+    else if(numero==0){
+        console.log(numero," o numero digitado é neutro")
+    }
+    else if((numero%2)==0){
+        console.log(numero," o numero digitado é par")
+    }
+    else{
+        console.log(numero," o numero digitado é impar")
+    }
+}
+
+mostraParImpar(13)
+
+function escreveLinha(tamanho=80){
+    for (letx=1; x<=tamanho; x++){
+        linha = linha + "-"
+    }
+    console.log(linha)
+}
+escreveLinha(80)
